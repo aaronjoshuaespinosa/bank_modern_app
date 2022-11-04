400dfebfe64fca4e04470d70403e77fc54a2f81a
eyrooonnn <espinosa.aaron45@gmail.com>
2022-11-04 16:07:32 +0800

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { footerLinks, socialMedia } from '../constants';
 const Footer = () => (
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
     <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
-      <div className='flex-1 flex flex-col justify-start my-10'>
+      <div className='flex-1 flex flex-col justify-start mr-10'>
         <img
           src={logo}
           alt='hoobank'
@@ -14,7 +14,7 @@ const Footer = () => (
         <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make the payments easy, reliable and secure.</p>
       </div>
 
-      <div className='flex-1.5 w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
+      <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
         {footerLinks.map((footerLink) => (
           <div
             key={footerLink.key}
@@ -35,15 +35,16 @@ const Footer = () => (
       </div>
     </div>
 
-    <div>
-      <p  className='font-poppins font-normal text-[18px] leading-[27px] text-white text-center'>2021 HooBank. All Rights Reserved.</p>
+    <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45]'>
+      <p  className='font-poppins font-normal text-[18px] leading-[27px] text-white text-center'>Copyright © 2021 HooBank. All Rights Reserved.</p>
 
       <div className='flex flex-row md:mt-0 mt-6'>
         {socialMedia.map((social, index) => (
           <img
           key={social.id}
           src={social.icon}
-          alt={social.id}/>
+          alt={social.id}
+          className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0'}`}/>
         ))}
       </div>
     </div>
